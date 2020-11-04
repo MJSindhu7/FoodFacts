e4b0b69946401ccea6c3d0676e86cd6a7d9857ee
MJSindhu7 <jayasindhu.manda@sjsu.edu>
2020-11-03 22:50:42 -0800

    Capturing image + Search image screens

---
+++ FoodFacts/SearchViewController.swift
@@ -0,0 +1,39 @@
+//
+//  SearchViewController.swift
+//  FoodFacts
+//
+//  Created by SindhuRam on 11/3/20.
+//  Copyright © 2020 archit. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    @IBAction func searchedItem(_ sender: Any) {
+    }
+    
+    @IBAction func getDetails(_ sender: Any) {
+    }
+    
+    @IBAction func onCancel(_ sender: Any) {
+        //self.dismiss(animated: true, completion: nil)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
